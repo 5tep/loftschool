d9d25d02de7ca8a0dd7f850a59ff64ea022d08f4
5tep <98393495+5tep@users.noreply.github.com>
2023-10-06 16:41:28 +0300

    Create fanction.php

---
+++ fanction.php
@@ -0,0 +1,39 @@
+<?php
+function task1($array, $concat = false){
+    if($concat){
+        return implode(",", $array);
+    }
+    switch($array as $key => $value){
+        echo "<p>$value</p>";
+    }
+}
+
+function task2(){
+
+}
+
+function task3(){
+
+}
+
+function task4(){
+
+}
+
+function task5(){
+
+}
+
+function task6(){
+
+}
+
+function task7(){
+
+}
+
+function task8(){
+
+}
+
+?>
