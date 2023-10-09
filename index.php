2738d3b96e72d7f8d1440651a09b4dee19ee7cfa
5tep <98393495+5tep@users.noreply.github.com>
2023-10-09 11:08:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <?php
 require('src/functions.php');
+//-----------
+$strings = ['one', 'two', '3'];
+task1($strings);
+echo task1($strings, true);
 
-task1();
+//---------
+task2('+', 3, 5, 0.5);
+task2('/', 3, 5, 0.5);
 
-task2();
+//----------
+task3(3,5);
 
-task3();
+//-----------
+$curdate = task4_1();
+$utime = task4_2('24.02.2016 00:00:00');
 
-task4_1();
-task4_2();
+//-----------
+$patter = 'Карл у Клары украл Кораллы';
+$twobottle = 'Две бутылки лимонада';
+$res = task5_1($patter);
+$threebottle = task5_2($twobottle);
 
-task5_1();
-
-task5_2();
-
-task6();
+//---------
+$fname = 'text.txt';
+$fp = fopen($fname, "w");
+fwrite($fp, $text);
+fclose($fp);
+$text_from_file = task6($fname);
 ?>
